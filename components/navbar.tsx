b1128bd62a3d5747bcf2b6b5a36c29264ee78e4d
Artem Yushkov <artem.yushkov@gmail.com>
2025-03-07 17:36:44 +0200

    Replace navbar.jsx with navbar.tsx for TypeScript migration and add logo component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,17 @@
 import { ModeToggle } from "@/components/mode-toogle";
+import SvgLogo from "@/components/logo";
+import Link from "next/link";
 
 // Navbar component fixed at the top with the title on the left and the toggle on the right.
 export const Navbar = () => {
     return (
         <div className="fixed top-0 left-0 right-0 p-1 flex justify-between items-center z-50 border-b bg-background">
-            <h1 className="ml-4">shryft.com</h1>
+            <Link href={"/"} className="h-fit w-fit">
+                <SvgLogo className="w-[100[px]] h-[50px]" />
+            </Link>
             <div className="mr-4">
                 <ModeToggle />
             </div>
-        </div>
+        </div >
     );
 };
