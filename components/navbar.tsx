b1b6a3c3262153abc975594236abf1843234e288
Artem Yushkov <artem.yushkov@gmail.com>
2025-03-20 08:21:55 +0200

    Localize navbar content with translations for Russian and Ukrainian

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -17,24 +17,30 @@ export const Navbar = ({ lang }: BasePageProps) => {
             {/* Top row with logo and images centered on the screen */}
             <div className="flex h-[130px] justify-center items-center">
                 <div className="flex items-center">
-                    <Link href={"/" + lang} className="flex justify-end items-center lg:pr-2 pr-2">
-                        <svg className="lg:w-[220px] w-[180px] h-[125px]" xmlns="http://www.w3.org/2000/svg">
-                            <image href="/logo.svg" width="100%" height="100%" />
-                        </svg>
+                    <Link href={"/" + lang} className="flex flex-col items-center lg:pr-2 pr-2">
+                        <Logo className="lg:w-[220px] w-[180px]" />
+                        <div className="text-center">
+                            <div className="text-xs lg:text-xs font-medium">
+                                {t("reliable_printing_partner")}
+                            </div>
+                            <div className="text-xs lg:text-xs font-medium">
+                                {t("always_near")}
+                            </div>
+                        </div>
                     </Link>
                     <img
                         className="hidden xl:block pr-4"
                         src="/banner.png"
                         alt="banner"
                     />
-                    <div className=" lg:w-[220px] flex flex-col self-end items-start">
+                    <div className=" lg:w-[220px] flex flex-col self-center items-start">
                         <div className="text-lg lg:text-xl font-bold tracking-wide">
-                            097 898 - 18 - 61
+                            {t("phone_number_1")}
                         </div>
                         <div className="text-lg lg:text-xl font-bold tracking-wide">
-                            093 150 - 28 - 29
+                            {t("phone_number_2")}
                         </div>
-                        <div className="text-base lg:text-lg font-bold">PRINT@SHRYFT.COM</div>
+                        <div className="text-base lg:text-lg font-bold">{t("email")}</div>
                     </div>
                 </div>
             </div >
