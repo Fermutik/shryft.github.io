8e93a4aba838360779904375f329aef6a8fd6b48
Artem Yushkov <artem.yushkov@gmail.com>
2025-03-26 15:51:11 +0200

    Enhance homepage banner with responsive design and text overlay for improved user engagement

--- app/_home.tsx
+++ app/_home.tsx
@@ -13,6 +13,7 @@ import path from "path";
 import matter from "gray-matter";
 import { remark } from 'remark';
 import html from 'remark-html';
+import { getT } from "@/lib/utils";
 
 export interface HomeItem {
     title: string;
@@ -135,6 +136,8 @@ export default async function HomePage({ lang }: BasePageProps) {
         return { ...item, cardsData: preparedCardsData };
     });
 
+    const t = getT(lang, "home");
+
 
     return (
         <SidebarProvider>
@@ -142,21 +145,23 @@ export default async function HomePage({ lang }: BasePageProps) {
             <SidebarInset>
                 <div className="flex flex-col justify-center items-center bg-gradient-to-r from-gray-50 to-gray-100">
                     <Navbar lang={lang} />
-                    <img
-                        src="/home/banner.png"
-                        srcSet="
-                            /home/banner.png 1920w,
-                            /home/banner-1536.png 1536w,
-                            /home/banner-1200.png 1200w,
-                            /home/banner-768.png 768w,
-                            /home/banner-600.png 600w,
-                        "
-                        sizes="(max-width: 720px) 100vw, (max-width: 960px) 100vw, 1920px"
-                        width="1920"
-                        height="600"
-                        alt="banner"
-                        className="alignnone size-full hidden lg:block"
-                    />
+                    <div className="relative w-full">
+                        {/* Banner image (without text) */}
+                        <img
+                            src="/home/banner.png"
+                            width="1920"
+                            height="600"
+                            alt="banner"
+                            className="w-full h-auto object-cover alignnone size-full hidden lg:block"
+                        />
+
+                        {/* Orange background and text overlay (spans the full width) */}
+                        <div className="absolute bottom-6 right-0 bg-orange-500 text-black px-4 py-2 text-start hidden lg:block">
+                            <p className="text-sm md:text-text-base lg:text-lg xl:text-xl 2xl:text-2xl px-4 whitespace-nowrap">
+                                {t("banner")}
+                            </p>
+                        </div>
+                    </div>
                     <CardsGrid lang={lang} item={homeItemsPrepared[0]} />
                     <CardsGrid lang={lang} item={homeItemsPrepared[1]} />
                     <CardsGrid lang={lang} item={homeItemsPrepared[2]} />
